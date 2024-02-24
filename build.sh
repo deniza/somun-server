5b45e350acb468947b02f958935cd802843f757e
Deniz Aydinoglu <deniz.aydinoglu@gmail.com>
2024-02-24 17:15:09 +0300

    added maven dependency check

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+function check_maven {
+  if ! [ -x "$(command -v mvn)" ]; then
+    echo "Error: Maven is not installed." >&2
+    exit 1
+  fi
+}
+
+check_maven
+
+echo "Building the application..."
+
 mvn clean install
