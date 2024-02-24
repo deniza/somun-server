6707d44a2d7d7f107fc024ce970541d038508f84
Deniz Aydinoglu <deniz.aydinoglu@gmail.com>
2024-02-24 17:15:31 +0300

    added java dependency check

--- somun.sh
+++ somun.sh
@@ -5,18 +5,33 @@
 # > somun setup   ## to setup the database
 # > somun run     ## to start the server application
 
+function check_java {
+  if ! [ -x "$(command -v java)" ]; then
+    echo "Error: Java is not installed." >&2
+    exit 1
+  fi
+}
+
+function print_usage {
+  echo "Usage: $0 [run|setup]"
+}
+
 function run {
+  check_java
   java -jar target/Somun-1.0.jar
 }
 
 function setup {
+  check_java
   java -jar target/Somun-1.0.jar --setup
 }
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 [run|setup]"
+  print_usage
 elif [  "$1" == "run" ]; then
   run
 elif [  "$1" == "setup" ]; then
   setup
+else
+  print_usage
 fi
