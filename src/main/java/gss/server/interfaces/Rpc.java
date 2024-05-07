8b690c2ef8369899967c2fc8d239416239b80593
Deniz Aydinoglu <deniz.aydinoglu@gmail.com>
2024-05-07 11:26:38 +0300

    added a test rpc function to call

--- src/main/java/gss/server/interfaces/Rpc.java
+++ src/main/java/gss/server/interfaces/Rpc.java
@@ -1,5 +1,6 @@
 package gss.server.interfaces;
 
+import gss.GssLogger;
 import gss.network.GssCallable;
 import gss.network.GssConnection;
 import gss.network.GssInterface;
@@ -11,6 +12,13 @@ public class Rpc extends GssInterface {
     public void call(String functionName, String jsonData, GssConnection con) {
 
         GameManager.get().getGameHandler().onRpcCall(functionName, jsonData);
+
+    }
+
+    @GssCallable
+    public void test(String p1, int p2, GssConnection con) {
+
+        GssLogger.info("[Rpc] test called p1: %s p2: %d", p1, p2);
 
     }
 
