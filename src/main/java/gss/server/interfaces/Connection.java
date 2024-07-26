4473d19cc05930eb4706de025a1b182eae90d556
Deniz Aydinoglu <deniz.aydinoglu@gmail.com>
2024-07-26 16:30:49 +0300

    added Connection interface to handle connect/disconnect from a central place

---
+++ src/main/java/gss/server/interfaces/Connection.java
@@ -0,0 +1,31 @@
+package gss.server.interfaces;
+
+import gss.network.GssConnection;
+import gss.network.GssInterface;
+import gss.server.manager.ConnectionManager;
+import gss.server.manager.GameManager;
+import gss.server.manager.PlayerManager;
+import gss.server.model.Player;
+
+public class Connection extends GssInterface  {
+
+    @Override
+    public void clientConnected(GssConnection con) {
+    }
+
+
+    @Override
+    public void clientDisconnected(GssConnection con) {
+
+        Player player = getPlayer(con);
+
+        if (player != null) {
+            GameManager.get().playerDisconnected(player);
+            PlayerManager.get().playerDisconnected(player);
+        }
+
+        ConnectionManager.get().unregister(con);
+
+    }
+
+}
